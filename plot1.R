8ac25f3876b27e85d30a224cdb556c9c360dd3c6
Jiao Zhang <jiaozhang@Jiaos-MacBook-Pro.local>
2016-11-23 14:41:15 -0800

    added pics and source files

---
+++ plot1.R
@@ -0,0 +1,6 @@
+data <- read.table('./household_power_consumption.txt',header=TRUE,sep=";",na.strings = "?")
+data$Date<-as.Date(data$Date,format="%d/%m/%Y")
+dataused<-subset(data,Date>='2007-02-01' & Date<='2007-02-02')
+png(file='plot1.png', width = 480, height = 480, units = "px")
+hist(dataused$Global_active_power,col = "red", main = "Global Active Power",xlab = "Global Active Power (kilowatts)")
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,7 @@
+data <- read.table('./household_power_consumption.txt',header=TRUE,sep=";",na.strings = "?")
+data$Date<-as.Date(data$Date,format="%d/%m/%Y")
+dataused<-subset(data,Date>='2007-02-01' & Date<='2007-02-02')
+dataused$Timenew<-as.POSIXct(paste(dataused$Date,dataused$Time), format="%Y-%m-%d %H:%M:%S")
+png(file='plot2.png', width = 480, height = 480, units = "px")
+plot(dataused$Timenew,dataused$Global_active_power,type = "l", xlab="",ylab = "Global Active Power (kilowatts)")
+dev.off()
